2fd65cedba6b8adf448a3801505fe424a27d73c4
aian <aian@qune.jp>
2020-03-09 16:33:55 +0900

    Add skelton of the mneca.KanjiCard class and main runner

---
+++ mneca/kanji.py
@@ -0,0 +1,13 @@
+##
+## Make Kanji Cards
+##
+
+class KanjiCard:
+
+    def __init__(self):
+        self.item_count_per_page = 10
+
+    def make(self, in_file = '', out_file = 'card.html'):
+        print("OK")
+        pass
+
---
+++ mneca/runner.py
@@ -0,0 +1,15 @@
+##
+## Main Routine
+##
+
+from kanji import KanjiCard
+
+
+#------------------------------------------------------------------------------#
+
+def main():
+    c = KanjiCard()
+    c.make()
+
+if __name__ == '__main__':
+    main()
